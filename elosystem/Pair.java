af46a58bf4674040378556e7a1ef4c261e3da6a3
lightarsi <lightars@gmail.com>
2018-11-11 17:18:31 +0300

    Licenses changed to be correct

--- elosystem/Pair.java
+++ elosystem/Pair.java
@@ -1,15 +1,12 @@
-/* Electric(tm) VLSI Design System
+/*
+ * Copyright (C) 2018 Lightars
  *
- * File: BinaryHeap.java
- *
- * Copyright (c) 2003, Oracle and/or its affiliates. All rights reserved.
- *
- * Electric(tm) is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Electric(tm) is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
